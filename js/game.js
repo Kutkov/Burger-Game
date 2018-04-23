1887001e43888c4fdfd15b32212067f674070db9
Max <max.k194@gmail.com>
2018-04-23 17:43:08 +0300

    fix double miss, add new border and body-wrapp

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
-var playerName = JSON.parse(localStorage.getItem('name'));
-console.log(playerName);
 // begin view
+// var playerName = JSON.parse(localStorage.getItem('name'));
+// console.log(playerName);
 
 var view = {
 	// Отображает счет
@@ -24,14 +24,15 @@ var view = {
 		elLogo.setAttribute('class', 'brend');
 	},
 	// Отображает блок с информацией перекрывающий игровое поле
-	showBodyWrapp: function() {
-		var bodyWrapp = document.getElementById('body-wrapp');
-		var btnNextRound = document.getElementById('next-round');
+	showBodyWrapp: function(wrapp, btn) {
+		var bodyWrapp = document.getElementById(wrapp);
+		var btnNextRound = document.getElementById(btn);
 		bodyWrapp.style.display = 'flex';
 		setTimeout(function() {
 			btnNextRound.style.visibility = 'visible';	
 		}, 1500);
 	},
+
 	// Удаляет все бургеры и логотипы(промохи) с поля
 	clearClass: function() {
 		var elCell = document.getElementsByTagName("td");
@@ -40,9 +41,9 @@ var view = {
 		}
 	},
 	// При нажатии кнопки нового раунда скрывает блок перекрывающий игровое поле
-	hideBodyWrapp: function() {
-		var bodyWrapp = document.getElementById('body-wrapp');
-		var btnNextRound = document.getElementById('next-round');
+	hideBodyWrapp: function(wrapp, btn) {
+		var bodyWrapp = document.getElementById(wrapp);
+		var btnNextRound = document.getElementById(btn);
 		bodyWrapp.style.display = 'none';
 		btnNextRound.style.visibility = 'hidden';	
 
@@ -105,15 +106,13 @@ var model = {
 				};
 			}
 		};
-		for (var j = -1; j < this.missShot.length; j++){
-			if(this.missShot[j] == id){
-				console.log('vot miss = ', this.missShot);
-			}else{
-				this.missShot[this.missShot.length] = id;
-				console.log('else');
-			}
-		};
-		
+
+		if(this.missShot.indexOf(id) == -1){
+			this.missShot[this.missShot.length] = id;
+		}else{
+			return false;
+		}
+
 		return id;
 
 	},
@@ -190,15 +189,12 @@ var model = {
 	}
 
 };
-
-
 // model end
 
 // begin controller
-
 var controller = {
 	// количество допустимых выстрелов
-	numShots: 15,
+	numShots: 30,
 	// Сгенерировать бургеры
 	createBurger: function () {
 		model.createMapBurger();
@@ -207,7 +203,8 @@ var controller = {
 	// Очистить поле и показать страницу поражения
 	clearMap: function() {
 		model.openMapBurger();
-		view.showBodyWrapp();
+		model.missShot.length = 0;
+		view.showBodyWrapp('body-wrapp', 'next-round');
 		setTimeout(function(){
 			view.clearClass();
 			controller.numShots = 10;
@@ -223,6 +220,8 @@ var controller = {
 
 			if (loss === true) {
 				view.showMsg("Хей! Этот бургер уже найден.");
+			} else if (loss === false) {
+				view.showMsg("Хех ты хочешь промахнуться дважды ? Ты явно не промах, друг.");
 			} else if (loss.status === 3) {
 				view.showBurger(loss.id, loss.color);
 				view.showMsg("Отлично! Этот сет найден.");
@@ -230,20 +229,18 @@ var controller = {
 				view.showBurger(loss.id, loss.color);
 				view.showMsg("Вот это Ketch up Burger");
 			} else if (typeof(loss) == 'string') {
-				console.log('loss = ', loss);
 				view.showLogo(loss);
-				view.showMsg(playerName+ ", " + "Ты достаточно мотивирован?");
+				view.showMsg("Ты достаточно мотивирован?");
 				this.numShots--;
 				view.showCount(this.numShots);
 				if(this.numShots === 0){
 					this.clearMap();
 				}
 			}
-			
 			if (model.destroyBurger === model.numBurger) {
-				// var count = Math.round((model.numBurger * 3 / this.numShots) * 1000);
+
 				view.showMsg("Ура! Ты нашел все Ketch up Burger и смог накормить друзей!");
-				// view.showCount(count);
+				view.showBodyWrapp('winner-page', 'next-level');
 			}
 		}
 	},
@@ -255,6 +252,7 @@ var controller = {
 	hoverClick: function (id) {
 		var el = document.getElementById(id);
 		var btnNextRound = document.getElementById('next-round');
+		var btnStartGame = document.getElementById('start-game');
 		el.onmouseover = function (e) {
 			e = e || window.event;
 
@@ -262,11 +260,8 @@ var controller = {
 				e.target.style.transition = '0.5s';
 				e.target.style.backgroundColor = 'rgba(104, 142, 218, 0.33)';
 
-
 				e.target.onclick = function () {
-					// var c = this.getAttribute('data-title');
 					var k = this.id;
-
 					controller.shotBurger(k);
 				};
 			}
@@ -280,7 +275,10 @@ var controller = {
 		};
 
 		btnNextRound.onclick = function () {
-			view.hideBodyWrapp();
+			view.hideBodyWrapp('body-wrapp', 'next-round');
+		};
+		btnStartGame.onclick = function () {
+			view.hideBodyWrapp('start-page', 'start-game');
 		};
 	}
 };
@@ -296,6 +294,11 @@ var controller = {
 		init: function () {
 			this.control();
 			this.event();
+			this.main();
+		},
+
+		main: function() {
+			view.showBodyWrapp('start-page', 'start-game');	
 		},
 
 		// control() - Запускаем необходимые методы объекта "controller"
